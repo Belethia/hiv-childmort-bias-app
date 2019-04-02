05b26d0635ddcb0d39a4eebe8693a8c4b84675bb
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2019-04-02 13:56:56 +0200

    Working version

--- app.R
+++ app.R
@@ -7,44 +7,150 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
 
-load("model.RData")
+library(shiny)
+library(shinydashboard)
+source("modules.R")
+
+load("best_fitting_model.RData")
+load("boot_models.RData")
+
+boot_models <- boot_models[1:350]
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  
+
+  mainPanel(
+    
+    column(width=12,
+           tags$h1("Population Characteristics"),
+           wellPanel(
+             fluidRow(
+               column(width=3,
+                      tags$h3("HIV prevalence"),
+                      numericInput("hiv1990",
+                                   "20 Year prior to survey:",
+                                   min = 0,
+                                   max = 1,
+                                   value = 0.5,
+                                   step=0.05,
+                                   width='110px'
+                      ),
+                      numericInput("hiv2000",
+                                   "10 Year prior to survey:",
+                                   min = 0,
+                                   max = 1,
+                                   value = 0.5,
+                                   step=0.05,
+                                   width='110px'
+                      ),
+                      numericInput("hiv2010",
+                                   "Year of survey:",
+                                   min = 0,
+                                   max = 1,
+                                   value = 0.5,
+                                   step=0.05,
+                                   width='110px'
+                      ),
+                      actionButton("compute","Submit",style="color: #fff; background-color: #982C25; border-color: #000000")
+                      
+               ),
+               column(width=3,
+                      tags$h3("ART prevalence*"),
+                      numericInput("art_prev2005",
+                                   "2005:",
+                                   min = 0,
+                                   max = 1,
+                                   value = 0.5,
+                                   step=0.05,
+                                   width='110px'
+                      ),
+                      numericInput("art_prev2007",
+                                   "2007:",
+                                   min = 0,
+                                   max = 1,
+                                   value = 0.5,
+                                   step=0.05,
+                                   width='110px'
+                      ),
+                      numericInput("art_prev2009",
+                                   "2009:",
+                                   min = 0,
+                                   max = 1,
+                                   value = 0.5,
+                                   step=0.05,
+                                   width='110px'
+                      )
+                      
+               ),
+               
+               column(3,
+                      tags$h3("Total Fertility Rate"),
+                      numericInput("tfr2000",
+                                   "10 Year prior to survey:",
+                                   min = 0,
+                                   max = 1,
+                                   value = 0.5,
+                                   step=0.05,
+                                   width='110px'
+                      ),numericInput("tfr2010",
+                                     "Year of survey",
+                                     min = 0,
+                                     max = 1,
+                                     value = 0.5,
+                                     step=0.05,
+                                     width='110px'
+                      )
+                      
+               ),
+               column(width=3,
+                      tags$h3("Under-5 mortality**"),
+                      numericInput("fiveq0_surv",
+                                   "",
+                                   min = 0,
+                                   max = 1,
+                                   value = 0.5,
+                                   step=0.05,
+                                   width='110px'
+                      )
+                      
+               )
+               
+             )
+           ),
+           tags$p("*ART prevalence: Percent of women aged 15-49 years who are on anti-retorviral therapy"),
+           tags$p("** Crude estimate of under-5 mortality for all age groups")
+           
+    ),
+    column(width=4,
+           tags$h1("Prediction"),
+           wellPanel(style = "background: white",
+             fluidRow(
+               predictionTableUI("myPredictionTable")
+             )
+           )
+           
+    )
+  ) 
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  observeEvent(input$compute,{
+    
+    age_label <- c("[15,20)","[20,25)","[25,30)","[30,35)","[35,40)","[40,45)","[45,49]")
+    
+    
+    newdata <- data.frame(agegroup=factor(1:7),fiveq0_surv=input$fiveq0_surv,hiv1990=input$hiv1990,hiv2000=input$hiv2000,hiv2010=input$hiv2010,art_prev2005=input$art_prev2005,art_prev2007=input$art_prev2007,art_prev2009=input$art_prev2009,tfr2000=input$tfr2000,tfr2010=input$tfr2010,age_label=age_label)
+    
+    callModule(predictionTable, "myPredictionTable", best.fitting.model,x,boot_models,full.model.formula, newdata)  
+    
+  })
+  
+  
+  
 }
 
 # Run the application 
---
+++ modules.R
@@ -0,0 +1,116 @@
+if(!require(flextable)){
+  install.packages("flextable",dependencies = TRUE,repos='http://cran.us.r-project.org')
+}
+
+
+require(flextable)
+
+
+if(!require(bcaboot)){
+  install.packages("bcaboot",dependencies = TRUE,repos='http://cran.us.r-project.org')
+}
+
+
+require(bcaboot)
+
+
+if(!require(glmnet)){
+  install.packages("glmnet",dependencies = TRUE,repos='http://cran.us.r-project.org')
+}
+
+
+require(glmnet)
+
+if (!require(magrittr)){
+  install.packages("magrittr",dependencies = TRUE,repos='http://cran.us.r-project.org')  
+}
+
+require(magrittr)
+
+if (!require(tidyverse)){
+  install.packages("tidyverse",dependencies = TRUE,repos='http://cran.us.r-project.org')  
+}
+
+require(tidyverse)
+
+
+##### predictionTable #####
+
+predictionTableUI <- function(id){
+  ns = NS(id)
+  
+  
+  tagList(uiOutput(ns("prediction_table")))
+  
+}
+
+
+predictionTable <- function(input,output,session,model,x,boot_models,model.formula,newdata){
+  
+  
+  output$prediction_table <- renderUI({
+    age_label <- c("[15,20)","[20,25)","[25,30)","[30,35)","[35,40)","[40,45)","[45,49]")
+    
+    to.predict <- newdata 
+    
+    to.predict <- model.matrix(model.formula,to.predict %>% mutate(corr=0))[,-1]
+    
+    z.value <- qnorm((1 + 0.95)/2)
+    
+    withProgress(message = 'Computing...', value = 0, {
+      prediction <- lapply(1:7,function(i){
+        # i<- 1
+        nx <- to.predict[i,,drop=FALSE]
+        t0 <- predict(model,newx = nx,s="lambda.min") %>% as.vector
+        tt <- boot_models %>%purrr::map(~predict(.,newx = nx,s="lambda.min") %>% as.vector) %>% unlist()
+        lambda <- model$lambda.min
+        theta <- as.matrix(coef(model, s = lambda))
+        pi_hat <- predict(model, newx = x, s = "lambda.min", type = "response")
+        n <- length(pi_hat)
+        
+        
+        
+        y_star <- (matrix(runif(n*length(tt)),ncol=length(tt)) <= matrix(rep(pi_hat,length(tt)),ncol=length(tt)))*1
+        
+        suff_stat <- t(y_star) %*% x
+        
+        r <- bcapar(t0=t0,tt=tt,bb=suff_stat)
+        
+        ci <- r$lims %>% as.data.frame() %>% mutate(quantile=as.numeric(rownames(.))) %>% filter(quantile %in% c(0.025,0.975)) %>% select(quantile,bca) %>% mutate(quantile=case_when(quantile==0.025 ~ "ci.lo",TRUE~ "ci.hi"),bca=bca++newdata$fiveq0_surv[i]) %>% spread(quantile,bca)
+        
+        r <- data.frame(agegroup=newdata$age_label[i], fit=t0+newdata$fiveq0_surv[i],ci)
+        
+        incProgress(1/7, detail = paste("Age ", age_label[i]))
+        
+        r
+      }) %>% bind_rows()
+      
+    })
+    prediction %<>% 
+      mutate(Output=sprintf("%0.2f\n(%0.2f; %0.2f)",fit,ci.lo,ci.hi)) %>% 
+      select(agegroup,Output)
+    
+    prediction %>% flextable() %>% 
+      add_footer(agegroup="Values in parenthesis are 95% prediction intervals.") %>%
+      merge_at(j=1:ncol(prediction),part="footer") %>%
+      set_header_labels(agegroup="Age Group") %>%
+      width(width=2)%>%
+      height(height = 0.75,part = "body")%>%
+      height(height = 0.75,part = "header")%>%
+      height(height = 0.75,part = "footer")%>%
+      bold(part="header")%>%
+      align(j=1,part="body",align = "left") %>%
+      align(j=2,part="body",align = "center") %>%
+      align(part="header",align = "center") %>%
+      fontsize(size=18,part = "body") %>%
+      fontsize(size=24,part = "header") %>%
+      fontsize(size=16,part = "footer") %>%
+      htmltools_value()
+  })
+  
+}
+
+destroy_predictionTable_observers <- function(values){
+  
+  
+}
